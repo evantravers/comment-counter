d40fbcc76d5ad9e0c7a8ffa434a16570352dc434
Evan Travers <evantravers@gmail.com>
2012-03-01 13:01:53 -0600

    most of the dumb design down

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source :rubygems
 gem "facebooker", "~> 1.0.75"
 gem 'sinatra', '1.3.2'
 gem 'thin', '1.3.1'
+gem "haml", "~> 3.1.4"
+gem "pry", "~> 0.9.8.2"
+gem "shotgun", "~> 0.9"
+gem "sass", "~> 3.1.15"
+gem "compass", "~> 0.11.7"
--- index.rb
+++ index.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
 
 get '/' do
-  "hello world"
+  haml :index
+end
+
+post '/' do
+  @post = params[:post]
+  haml :success
+end
+
+# style sheet rules
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
 end
