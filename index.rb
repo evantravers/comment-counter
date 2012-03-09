1bdfc1ab0bdb5e069d72eb639df694f190bf7b41
Evan Travers <evantravers@gmail.com>
2012-03-09 15:21:20 -0600

    why doesn't it show up

--- index.rb
+++ index.rb
@@ -83,6 +83,7 @@ post '/' do
   end
   @words = @words.to_a.sort_by! {|k,v| v}.reverse
   @e_class, @emotion = eval_emotion(@emotion_index)
+  puts "class = #{@e_class}, words = #{@emotion}"
   haml :success
 end
 
